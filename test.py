a1760c246d8b850d574407cd25facf0f19d8463c
EllaYu310 <ellay9@nycstudents.net>
2023-09-15 02:03:26 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,3 +7,41 @@ def message(input):
     print(input)
 message("Hello world!")
 message("Goodbye.")
+
+
+import turtle
+
+t = turtle
+t.shape("turtle")
+
+t.bgcolor("yellow")
+t.color("blue", "black") 
+t.begin_fill()
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200)
+
+t.penup()
+t.left(90)
+t.forward(300)
+t.pendown()
+def square(x):
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+    t.left(90)
+    t.forward(x)
+square(200)
+
+t.end_fill()
+
+turtle.done()
+
